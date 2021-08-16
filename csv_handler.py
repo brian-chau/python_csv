d4d14611ca35f1ec0ef0c72652417b8708f4c3d3
Brian Chau <brian.chau@outlook.com>
2021-08-15 17:55:46 -0700

    Re-introduced yield, added fstring, removed hard-coded filename even though this script was tailored for the single file.

--- csv_handler.py
+++ csv_handler.py
@@ -1,13 +1,12 @@
 import csv
 import sys
 
-def func(filename):
+def pickup_dropoff_yielder(filename):
     with open(filename) as csv_file:
         dr = csv.reader(csv_file)
         is_first_line = True
         payment_type_index = 0
         pu_index, do_index = 0, 0
-        pu_zone_cnt, do_zone_cnt = 0,0
         for row in dr:
             if is_first_line:
                 payment_type_index = row.index('payment_type')
@@ -15,13 +14,21 @@ def func(filename):
                 do_index = row.index('DOLocationID')
                 is_first_line = False
             elif len(row) > 0 and row[payment_type_index] == '3':
-                if row[pu_index] == '170':
-                    pu_zone_cnt += 1
-
-                elif row[do_index] == '170':
-                    do_zone_cnt += 1
-        print(pu_zone_cnt)
-        print(do_zone_cnt)
+                yield row[pu_index], row[do_index]
 
 if __name__ == '__main__':
-    func('yellow_tripdata_2017-03.csv')
+    if len(sys.argv) != 2:
+        print(f"Expecting a filename! Received {len(sys.argv) - 1} command line arguments.")
+        sys.exit(0)
+
+    filename = sys.argv[1]
+    pu_zone_cnt, do_zone_cnt = 0,0
+    for pu, do in pickup_dropoff_yielder(filename):
+        if pu == '170':
+            pu_zone_cnt += 1
+        elif do == '170':
+            do_zone_cnt += 1
+    print(pu_zone_cnt)
+    print(do_zone_cnt)
+
+
