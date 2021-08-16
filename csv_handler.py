f072a57f851294f97bd753aff00dd56fadf8060e
Brian Chau <brian.chau@outlook.com>
2021-08-15 17:46:28 -0700

    Unfortunately removed yield since it was unnecessary, but replaced the csv.DictReader with a csv.reader and indexed the columns in order to bring the run time down from 26 seconds to 12 seconds

--- csv_handler.py
+++ csv_handler.py
@@ -3,18 +3,25 @@ import sys
 
 def func(filename):
     with open(filename) as csv_file:
-        dr = csv.DictReader(csv_file)
+        dr = csv.reader(csv_file)
+        is_first_line = True
+        payment_type_index = 0
+        pu_index, do_index = 0, 0
+        pu_zone_cnt, do_zone_cnt = 0,0
         for row in dr:
-            if row['payment_type'] in ['3']:
-                yield row
+            if is_first_line:
+                payment_type_index = row.index('payment_type')
+                pu_index = row.index('PULocationID')
+                do_index = row.index('DOLocationID')
+                is_first_line = False
+            elif len(row) > 0 and row[payment_type_index] == '3':
+                if row[pu_index] == '170':
+                    pu_zone_cnt += 1
+
+                elif row[do_index] == '170':
+                    do_zone_cnt += 1
+        print(pu_zone_cnt)
+        print(do_zone_cnt)
 
 if __name__ == '__main__':
-    pu_zone_cnt, do_zone_cnt = 0,0
-    for row in func('yellow_tripdata_2017-03.csv'):
-        if row['PULocationID'] == '170':
-            pu_zone_cnt += 1
-
-        elif row['DOLocationID'] == '170':
-            do_zone_cnt += 1
-    print(pu_zone_cnt)
-    print(do_zone_cnt)
+    func('yellow_tripdata_2017-03.csv')
